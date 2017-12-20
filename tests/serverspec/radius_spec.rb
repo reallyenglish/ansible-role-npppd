fa158bc0537cf7a89ac736e8ffb08e93de467a3d
Mitsuru Y <mitsuruy@reallyenglish.com>
2017-12-20 13:45:39 +0900

    [bugfix] fix thirdparty repo, bump rubocop and QA

--- tests/serverspec/radius_spec.rb
+++ tests/serverspec/radius_spec.rb
@@ -1,12 +1,12 @@
-require 'spec_helper'
-require 'serverspec'
+require "spec_helper"
+require "serverspec"
 
-package = 'npppd'
-service = 'npppd'
-config  = '/etc/npppd/npppd.conf'
-user    = '_npppd'
-group   = '_npppd'
-ports   = [ 1701 ]
+package = "npppd"
+service = "npppd"
+config  = "/etc/npppd/npppd.conf"
+user    = "_npppd"
+group   = "_npppd"
+ports   = [1701]
 
 describe file(config) do
   it { should be_file }
@@ -14,16 +14,16 @@ describe file(config) do
   # tunnel l2tp_tunnel protocol l2tp {
   #   listen on 10.0.2.15
   # }
-  # 
+  #
   its(:content) { should match /^ipcp ipcp1 \{\n\s+pool-address 192\.168\.100\.1-192\.168\.100\.250\n\s+dns-servers 8\.8\.8\.8\n\}$/ }
   # ipcp ipcp1 {
   #   pool-address 192.168.100.1-192.168.100.250
   #   dns-servers 8.8.8.8
   # }
-  # 
+  #
   its(:content) { should match /^interface pppx0 address 192\.168\.100\.254 ipcp ipcp1$/ }
   # interface pppx0 address 192.168.100.254 ipcp ipcp1
-  # 
+  #
   its(:content) { should match /^authentication RADIUS type radius \{\n\s+strip-nt-domain no\n\s+authentication-server {\n\s+address 127\.0\.0\.1 port 1812 secret "password"\n\s+timeout 10\n\s+}\n}/ }
   # authentication RADIUS type radius {
   #   authentication-server {
@@ -32,7 +32,6 @@ describe file(config) do
   # }
   its(:content) { should match /^bind tunnel from l2tp_tunnel authenticated by RADIUS to pppx0$/ }
   # bind tunnel from l2tp_tunnel authenticated by RADIUS to pppx0
-
 end
 
 describe service(service) do
@@ -42,6 +41,6 @@ end
 
 ports.each do |p|
   describe port(p) do
-    it { should be_listening.with('udp') }
+    it { should be_listening.with("udp") }
   end
 end

4141b3fe58dd0144534452dbe1e19b4d459bf5ae
mitsururike <mitsururike2@gmail.com>
2017-12-25 15:34:56 +0900

    [bugfix] fix third party repo, bump rubocop and QA (#13)
    * [bugfix] fix thirdparty repo, bump rubocop and QA
    
    * fix the failed previous commit

--- tests/integration/server_client/spec/spec_helper.rb
+++ tests/integration/server_client/spec/spec_helper.rb
@@ -1,10 +1,10 @@
-require 'infrataster/rspec'
-require 'capybara'
+require "infrataster/rspec"
+require "capybara"
 
-ENV['VAGRANT_CWD'] = File.dirname(__FILE__)
-ENV['LANG'] = 'C'
+ENV["VAGRANT_CWD"] = File.dirname(__FILE__)
+ENV["LANG"] = "C"
 
-if ENV['JENKINS_HOME']
+if ENV["JENKINS_HOME"]
   # XXX "bundle exec vagrant" fails to load.
   # https://github.com/bundler/bundler/issues/4602
   #
@@ -21,36 +21,35 @@ if ENV['JENKINS_HOME']
   # include the path of bin to vagrant
   vagrant_real_path = `pkg info -l vagrant | grep -v '/usr/local/bin/vagrant' | grep -E 'bin\/vagrant$'| sed -e 's/^[[:space:]]*//'`
   vagrant_bin_dir = File.dirname(vagrant_real_path)
-  ENV['PATH'] = "#{vagrant_bin_dir}:#{ENV['PATH']}"
+  ENV["PATH"] = "#{vagrant_bin_dir}:#{ENV['PATH']}"
 end
 
 Infrataster::Server.define(
   :gw1,
-  '192.168.21.254',
+  "192.168.21.254",
   vagrant: true
 )
 
 Infrataster::Server.define(
   :client1,
-  '172.16.0.100',
+  "172.16.0.100",
   vagrant: true
 )
 
 Infrataster::Server.define(
   :client2,
-  '192.168.21.100',
+  "192.168.21.100",
   vagrant: true
 )
 
-
 def fetch(uri_str, limit = 10)
-  raise ArgumentError, 'too many HTTP redirects' if limit == 0
+  raise ArgumentError, "too many HTTP redirects" if limit == 0
   response = Net::HTTP.get_response(URI(uri_str))
   case response
   when Net::HTTPSuccess then
     response
   when Net::HTTPRedirection then
-    location = response['location']
+    location = response["location"]
     warn "redirected to #{location}"
     fetch(location, limit - 1)
   else
@@ -58,23 +57,26 @@ def fetch(uri_str, limit = 10)
   end
 end
 
-def retry_and_sleep(options = {}, &block)
+def retry_and_sleep(options = {})
   opts = {
-    :tries => 60,
-    :sec => 10,
-    :on => [ Exception ],
-    :verbose => false
+    tries: 60,
+    sec: 10,
+    on: [Exception],
+    verbose: false
   }.merge(options)
-  tries, sec, on, verbose = opts[:tries], opts[:sec], opts[:on], opts[:verbose]
+  tries = opts[:tries]
+  sec = opts[:sec]
+  on = opts[:on]
+  verbose = opts[:verbose]
   i = 1
   begin
     yield
   rescue *on => e
-    warn "rescue an excpetion %s" % [ e.class ] if verbose
+    warn format("rescue an excpetion %s", e.class) if verbose
     warn e.message if verbose
     if (tries -= 1) > 0
-      warn "retrying (remaining: %d)" % [ tries ]
-      warn "sleeping %d sec" % [ sec ] if verbose
+      warn format("retrying (remaining: %d)", tries)
+      warn format("sleeping %d sec", sec) if verbose
       sleep sec
       i += 1
       retry
@@ -82,9 +84,8 @@ def retry_and_sleep(options = {}, &block)
   end
 end
 
-require 'shellwords'
+require "shellwords"
 class Vagrant
-
   def initialize
     @status # Process::Status
     @out
@@ -92,26 +93,26 @@ class Vagrant
   end
 
   def up(server)
-    execute('up', '', server)
+    execute("up", "", server)
   end
 
   def boot(server)
-    execute('up', '--no-provision', server)
+    execute("up", "--no-provision", server)
   end
 
   def provision(server)
-    execute('provision', '', server)
+    execute("provision", "", server)
   end
 
   def destroy(server)
-    execute('destroy', '--force', server)
+    execute("destroy", "--force", server)
   end
 
   def execute(command, opt, server)
     begin
-      command = "vagrant #{Shellwords.escape(command)}" + ' '
-      command += "#{Shellwords.escape(opt)}" + ' ' if not opt.empty?
-      command += "#{Shellwords.escape(server)}"
+      command = "vagrant #{Shellwords.escape(command)}" + " "
+      command += Shellwords.escape(opt).to_s + " " unless opt.empty?
+      command += Shellwords.escape(server).to_s
       @out, @err, @status = Open3.capture3(command)
     rescue SystemCallError => e
       @status = e
@@ -119,9 +120,7 @@ class Vagrant
     status
   end
 
-  def status
-    @status
-  end
+  attr_reader :status
 
   def stdout
     @out
@@ -139,12 +138,10 @@ class Vagrant
     @err.split("\n").to_a
   end
 
-  def status
-    @status
-  end
+  attr_reader :status
 
   def success?
-    return  false if @success.nil?
+    return false if @success.nil?
     return false if @status.is_a?(Exception)
     @status.success?
   end
--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,13 +1,13 @@
-require 'spec_helper'
-require 'serverspec'
+require "spec_helper"
+require "serverspec"
 
-package = 'npppd'
-service = 'npppd'
-config  = '/etc/npppd/npppd.conf'
-user    = '_npppd'
-group   = '_npppd'
-ports   = [ 1701 ]
-users_file  = '/etc/npppd/npppd-users'
+package = "npppd"
+service = "npppd"
+config  = "/etc/npppd/npppd.conf"
+user    = "_npppd"
+group   = "_npppd"
+ports   = [1701]
+users_file = "/etc/npppd/npppd-users"
 
 describe file(config) do
   it { should be_file }
@@ -15,23 +15,22 @@ describe file(config) do
   # tunnel l2tp_tunnel protocol l2tp {
   #   listen on 10.0.2.15
   # }
-  # 
+  #
   its(:content) { should match /^ipcp ipcp1 \{\n\s+pool-address 192\.168\.100\.1-192\.168\.100\.250\n\s+dns-servers 8\.8\.8\.8\n\}$/ }
   # ipcp ipcp1 {
   #   pool-address 192.168.100.1-192.168.100.250
   #   dns-servers 8.8.8.8
   # }
-  # 
+  #
   its(:content) { should match /^interface pppx0 address 192\.168\.100\.254 ipcp ipcp1$/ }
   # interface pppx0 address 192.168.100.254 ipcp ipcp1
-  # 
+  #
   its(:content) { should match /^authentication LOCAL type local \{\n\s+users-file "\/etc\/npppd\/npppd-users"\n\}$/ }
   # authentication LOCAL type local {
   #   users-file "/etc/npppd/npppd-users"
   # }
   its(:content) { should match /^bind tunnel from l2tp_tunnel authenticated by LOCAL to pppx0$/ }
   # bind tunnel from l2tp_tunnel authenticated by LOCAL to pppx0
-
 end
 
 describe file(users_file) do
@@ -39,7 +38,6 @@ describe file(users_file) do
   its(:content) { should match /^foo:\s+\\\n\s+:password=password\\c\\\\\\\^:\\/ }
   its(:content) { should match /bar-:\s+\\\n\s+:password=password:\\\n\s+:framed-ip-address=192\.168\.100\.1:\\$/ }
   its(:content) { should match /buz_:\s+\\\n\s+:password=password:\\\n\s+:framed-ip-network=192\.168\.101\.0\/24:$/ }
-
 end
 
 describe service(service) do
@@ -49,6 +47,6 @@ end
 
 ports.each do |p|
   describe port(p) do
-    it { should be_listening.with('udp') }
+    it { should be_listening.with("udp") }
   end
 end
